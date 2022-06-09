c2c159432deb8b926747a7f351efa38ed4ae1034
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-09 11:13:11 -0400

    commit tree ae68d6994c45f9b7d6d118fd442e80d4d9a78a94

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    # rescue_from ActiveRecord::RecordInvalid, with: :error_message_invalid
+
+    # private
+
+    # def error_message_not_found (e)
+    #     render json: {errors: e}, status: :not_found
+    # end
+
+    # def error_message_invalid (e)
+    #     render json: {errors: e}, status: :unprocessable_entity
+    # end
+
 end
